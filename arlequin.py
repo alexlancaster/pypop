0e19d9fec31924d14b57546b5f380f0b39d23b3a
alex <>
2001-03-30 09:55:27 +0000

    * Haplo.py:  Generate files needed by Arlequin for standalone   execution.  Invoke "arlecore.exe" via an os.system call. * Modify driver script to generate arl_run.txt, arl_run.ars runtime   files and the Arlequin `.arp' project output with the samples,   invoke the Haplo.runArlequin method to generate the output.

--- arlequin.py
+++ arlequin.py
@@ -3,13 +3,19 @@
 import sys
 from Haplo import HaploArlequin
 
-f = open(sys.argv[1], 'r')
-data = f.readlines()
+inputFilename = sys.argv[1]
+arpFilename = sys.argv[2]
 
 haploParse = HaploArlequin(idCol = 1,
+                           arpFilename = arpFilename,
                            prefixCols = 6,
                            suffixCols = 1,
                            windowSize = 3,
                            debug=1)
 
+f = open(inputFilename, 'r')
+data = f.readlines()
+f.close()
+
 haploParse.outputArlequin(data)
+haploParse.runArlequin()
