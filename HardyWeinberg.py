2aaedfe5afb9006f087ba8ac38be196df7c55435
Mark P. Nelson <mpn@allele5.biol.berkeley.edu>
2001-05-03 23:46:16 +0000

    Check in first, non-working start to HardyWeinberg module.

---
+++ HardyWeinberg.py
@@ -0,0 +1,22 @@
+class HardyWeinberg:
+  """Given the observed genotypes for a locus,
+
+  calculate the expected genotype counts based on Hardy Weinberg proportions
+  for individual genotype values, and test for fit."""
+
+  def __init__(self, locusData, debug=0):
+  """Constructor.
+
+  - locusData to be provided by driver script via a
+    call to ParseFile.getLocusData(locus)."""
+
+    self.locusData = locusData
+    self.debug = debug
+
+    if self.debug:
+      print self.locusData
+      for item in self.locusData:
+        print item
+
+    def getSampleSize(self):
+      return(len(self.locusData))
