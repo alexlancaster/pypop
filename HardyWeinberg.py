6f92987ee6d48245600236da03376c74b53650ae
Mark P. Nelson <mpn@allele5.biol.berkeley.edu>
2001-05-09 19:56:50 +0000

    Altered debugging output in HW.
    Adapted the summary script to create tdw.py, the test drive wrapper

--- HardyWeinberg.py
+++ HardyWeinberg.py
@@ -9,21 +9,32 @@ class HardyWeinberg:
 
   Given the observed genotypes for a locus, calculate the expected
   genotype counts based on Hardy Weinberg proportions for individual
-  genotype values, and test for fit."""
+  genotype values, and test for fit.
 
-  def __init__(self, locusData, debug=0):
+  """
+
+  def __init__(self, locusData, alleleCount, debug=0):
     """Constructor.
 
     - locusData to be provided by driver script via a
-      call to ParseFile.getLocusData(locus)."""
+      call to ParseFile.getLocusData(locus).
+
+    """
 
     self.locusData = locusData
+    self.alleleCount = alleleCount
     self.debug = debug
 
     if self.debug:
-      print self.locusData
-      for item in self.locusData:
-        print item
+      for genotype in self.locusData:
+        print genotype
 
   def getSampleSize(self):
+    """Returns N, the number of records
+
+    """
     return(len(self.locusData))
+
+#  """Assign the number of alleles, two per person."""
+#  self.k = 2 * HardyWeinberg.getSampleSize()
+
