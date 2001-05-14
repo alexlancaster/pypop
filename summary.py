85eb3fe8fc3c4e223ccdfdb196eb8f0ef4b387b9
Alex Lancaster <alexl@berkeley.edu>
2001-05-14 22:28:42 +0000

    Emit script usage if incorrect number of args give. Add short documentation on how to use the {Text,XML}StreamOutput object.

--- summary.py
+++ summary.py
@@ -4,7 +4,10 @@ from ParseFile import ParseGenotypeFile
 from Utils import XMLOutputStream, TextOutputStream
 
 # read in IHWG and parse data file from first argument to created
-# object
+# object, print usage if number of arguments is incorrect
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: summary.py <datafile>")
 
 parsefile = ParseGenotypeFile (sys.argv[1],
                                alleleDesignator='*',
@@ -12,6 +15,22 @@ parsefile = ParseGenotypeFile (sys.argv[1],
                                debug=0)
 
 # serialize summary info for population
+
+"""the argument to the serialize* methods should be a
+{Text,XML}OutputStream, which is created from a file object.
+Currently this is simply stdout, so to serialize the output to a file,
+you need to create a file handle to pass to the
+{Text,XML}OutputStream, e.g.:
+
+xmlOutStream = XMLOutputStream(open('output.xml', 'w'))
+parsefile.serializeMetadataTo(xmlOutStream)
+parsefile.serializeAlleleCountTo(xmlOutStream)
+xmlOutStream.close()
+
+Remember not to close() the stream output until the entire data you
+want to dump to the file has been written.
+
+"""
 parsefile.serializeMetadataTo(TextOutputStream(sys.stdout))
 
 # serialize the allele count in text form to stdout
