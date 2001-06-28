7fd3f87ab2c36b1367e9933d99124270015296c0
mpn <>
2001-06-28 18:08:56 +0000

    Clarified comments. Tightened a couple of loops. Completed initial HW chi squared calculations

--- HardyWeinberg.py
+++ HardyWeinberg.py
@@ -4,7 +4,7 @@
 
 """
 
-import string
+import string, sys
 
 class HardyWeinberg:
   """Calculate Hardy-Weinberg statistics.
@@ -36,6 +36,7 @@ class HardyWeinberg:
     self.k = 2 * self.n
 
     self._generateTables()
+    self._calcChisq()
 
 #     if self.debug:
 #       self.counter = 0
@@ -76,6 +77,7 @@ class HardyWeinberg:
     for genotype in self.locusData:
       """Run through each tuple in the given genotype data and
       create a dictionary of allele counts"""
+
       self.alleleTotal += 2
       if self.alleleCounts.has_key(genotype[0]):
         self.alleleCounts[genotype[0]] += 1
@@ -97,12 +99,14 @@ class HardyWeinberg:
     for allele in self.alleleCounts.keys():
       """For each entry in the dictionary of allele counts
       generate a corresponding entry in a dictionary of frequencies"""
+
       freq = self.alleleCounts[allele] / float(self.k)
       frequencyAccumulator += freq
       self.alleleFrequencies[allele] = freq
 
     for genotype in self.observedGenotypes:
       """Generate a dictionary of genotype:count key:values"""
+
       # maybe this should be a copy of possibleGenotypes with zeroes where
       # there are no observations???
       if self.observedGenotypeCounts.has_key(genotype):
@@ -112,32 +116,91 @@ class HardyWeinberg:
 
     for i in range(len(self.observedAlleles)):
       """Generate a list of all possible genotypes"""
-      self.possibleGenotypes.append(self.observedAlleles[i] + ":" + self.observedAlleles[i])   # the homozygotes
-      for j in range(i + 1, len(self.observedAlleles)):
-        if j < len(self.observedAlleles):
+
+      for j in range(i, len(self.observedAlleles)):
           self.possibleGenotypes.append(self.observedAlleles[i] + ":" + self.observedAlleles[j])
 
-#--mpn-- is working here
     for genotype in self.possibleGenotypes:
-      """Calculate expected genotype counts under HWP"""
+      """Calculate expected genotype counts under HWP
+
+      - Create a dictionary of genotype:frequency key:values"""
+
       temp = string.split(genotype, ':')
       if temp[0] == temp[1]:         # homozygote, N * pi * pi
         self.expectedGenotypeCounts[genotype] = self.n * self.alleleFrequencies[temp[0]] * self.alleleFrequencies[temp[1]]
       else:                          # heterozygote, 2N * pi * pj
         self.expectedGenotypeCounts[genotype] = 2 * self.n * self.alleleFrequencies[temp[0]] * self.alleleFrequencies[temp[1]]
 
+    total = 0
+    for value in self.expectedGenotypeCounts.values():
+      """Check that the sum of expected genotype counts approximates N"""
 
-    if self.debug:
-      print 'Allele Frequencies:'
-      for allele in self.alleleFrequencies.items():
-        print allele
-      print 'Cumulative frequency:', frequencyAccumulator
-      print 'Total allele count:', self.alleleTotal
-      print '\nGenotype counts:'
-      print 'Observed:'
-      for genotype in self.observedGenotypeCounts.items():
-        print genotype
-      print 'Expected:'
-      for genotype in self.expectedGenotypeCounts.items():
-        print genotype
+      total += value
+    if abs(float(self.n) - total) > float(self.n) / 1000.0:
+      print 'AAIIEE!'
+      print 'Calculated sum of expected genotype counts is:', total, ', but N is:', self.n
+      sys.exit()
+
+#     if self.debug:
+#       print 'Allele Frequencies:'
+#       for allele in self.alleleFrequencies.items():
+#         print allele
+#       print 'Cumulative frequency:', frequencyAccumulator
+#       print 'Total allele count:', self.alleleTotal
+#       print '\nGenotype counts:'
+#       print 'Possible:'
+#       for genotype in self.possibleGenotypes:
+#         print genotype
+#       print 'Observed:'
+#       for genotype in self.observedGenotypeCounts.items():
+#         print genotype
+#       print 'Expected:'
+#       for genotype in self.expectedGenotypeCounts.items():
+#         print genotype
+
+  def _calcChisq(self):
+    """Calculate the chi-squareds"""
+
+    printExpected = [] # list flagging genotypes worth printing
+    chisq = {}
+    commonGenotypeCounter = 0
+    rareGenotypeCounter = 0
+
+    print 'Calculating Chi Squared'
+
+    #--mpn--
+    for genotype in self.expectedGenotypeCounts.keys():
+      if self.expectedGenotypeCounts[genotype] > 4.99:
+        if self.debug:
+          print 'Expected:'
+          print genotype, self.expectedGenotypeCounts[genotype]
+          if self.observedGenotypeCounts.has_key(genotype):
+            print 'Observed:', self.observedGenotypeCounts[genotype]
+          else:
+            print 'Observed: 0'
+
+        printExpected.append(genotype)
+
+        commonGenotypeCounter += 1
+        if self.observedGenotypeCounts.has_key(genotype):
+          observedCount = self.observedGenotypeCounts[genotype]
+        else:
+          observedCount = 0.0
+        degreesOfFreedomAccumulator = commonGenotypeCounter - 1
+        chisq[genotype] = ((observedCount - \
+                          self.expectedGenotypeCounts[genotype]) * \
+                          (observedCount - \
+                          self.expectedGenotypeCounts[genotype])) /\
+                          self.expectedGenotypeCounts[genotype]
+        if self.debug:
+          print 'Chi Squared value:'
+          print genotype, ':', chisq[genotype]
+
+      else:
+        """Expected genotype count for this genotype is less than 5"""
+        # do not append this genotype to the printExpected list
+        rareGenotypeCounter += 1
+        pass
+
+
 
