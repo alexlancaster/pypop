ba6c3c28ea7c8b0f62c85280194395f166d3ac60
alex <>
2002-03-02 04:08:28 +0000

    Generate the meta wrapper XML file using a single document with ENTITYs to include data directly.

--- meta.py
+++ meta.py
@@ -4,12 +4,33 @@ import os, sys, libxsltmod
 files = sys.argv[1:]
 
 # generate a metafile XML wrapper
-meta_string="<meta>"
+
+# open doctype
+meta_string="<!DOCTYPE meta [\n"
+entities = ""
+includes = ""
+
 for f in files:
-    meta_string += "<filename>%s</filename>" % f
+    base = os.path.basename(f)
+    entities += "<!ENTITY %s SYSTEM \"%s\">\n" % (base, f)
+    includes += "&%s;\n" % base
+
+# put entities after doctype
+meta_string += entities
+
+# close doctype
+meta_string += "]>\n"
+
+# open tag
+meta_string += "<meta>\n"
+
+# include content
+meta_string += includes
+
+# close tag
 meta_string += "</meta>"
 
-print meta_string
+#print meta_string
 
 # do the transformation
 output = libxsltmod.translate_to_string('f', 'meta.xsl',
