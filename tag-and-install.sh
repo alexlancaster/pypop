d3ff90e7edf0411d8b20406a0a24643ed4a9e0d1
Alex Lancaster <alexl@berkeley.edu>
2002-03-14 00:36:42 +0000

    Update.

--- tag-and-install.sh
+++ tag-and-install.sh
@@ -1,8 +1,19 @@
 #/bin/sh
-VERSION=VERSION_FOR_RUN-$(date +%Y-%m-%d)
+echo -n "If you are certain that module is completely up-to-date with CVS (run 'cvs update'), enter y: "
+read UPTODATE
+test $UPTODATE != "y" && exit
+test -f VERSION && rm VERSION
+DATE=$(date +%Y-%m-%d)
+echo -n "Enter the CVS tag prefix (only alphanumeric, _ or - allowed): "
+read TAG_PREFIX
+VERSION=${TAG_PREFIX}-${DATE}
+echo "CVS tag is $VERSION"
 echo tagging pypop with $VERSION
-echo "<version>$VERSION</version>" > version.xml
+echo cvs tag $VERSION
+echo $VERSION > VERSION
 echo building $VERSION
-./setup.py build
+echo ./setup.py build
 echo installing $VERSION
 echo sudo ./setup.py install
+echo rm VERSION
+
