9a6bf936ae43d3f49f484fea4fdf9a19f1a37b61
alex <>
2002-03-14 00:40:22 +0000

    Add message to user to check for being up-to-date with repository.

--- tag-and-install.sh
+++ tag-and-install.sh
@@ -1,7 +1,10 @@
 #/bin/sh
 echo -n "If you are certain that module is completely up-to-date with CVS (run 'cvs update'), enter y: "
 read UPTODATE
-test $UPTODATE != "y" && exit
+if test x$UPTODATE != xy; then
+    echo "run 'cvs update'"
+    exit
+fi
 test -f VERSION && rm VERSION
 DATE=$(date +%Y-%m-%d)
 echo -n "Enter the CVS tag prefix (only alphanumeric, _ or - allowed): "
