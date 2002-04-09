addede8770dc564919daab4962e2b1d0cdb56e1d
Alex Lancaster <alexl@berkeley.edu>
2002-04-09 04:07:44 +0000

    Start on porting libxsltmod call to built-in bindings for libxml2/libxslt.  Not quite working yet.

--- meta.py
+++ meta.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 import os, sys, libxsltmod
+from getopt import getopt, GetoptError
 
-files = sys.argv[1:]
+usage_message = """Usage: meta.py [OPTION] INPUTFILES...
+
+Processes INPUTFILES and generates 'meta'-analyses.  INPUTFILES are
+expected to be the XML output files taken from runs of 'pypop'.
+
+  -m, --meta-xslt=FILE    use specified XSLT FILE (default: 'xslt/meta.xsl')
+  -h, --help              show this message
+
+  INPUTFILES  input XML files""" 
+
+try:
+  opts, args =getopt(sys.argv[1:],"m:h", ["meta-xslt=", "help"])
+except GetoptError:
+  sys.exit(usage_message)
+
+metaXSLTFilename= 'xslt/meta.xsl'
+
+# parse options
+for o, v in opts:
+  if o in ("-m", "--meta-xslt"):
+    metaXSLTFilename = v
+  elif o in ("-h", "--help"):
+    sys.exit(usage_message)
+
+print metaXSLTFilename
+
+# parse arguments
+files = args
+#files = sys.argv[1:]
 
 # generate a metafile XML wrapper
 
@@ -33,7 +62,34 @@ meta_string += "</meta>"
 #print meta_string
 
 # do the transformation
-output = libxsltmod.translate_to_string('f', 'meta.xsl',
-                                        's', meta_string)
 
-print output
+import libxml2
+import libxslt
+
+# parse the stylesheet file
+styledoc = libxml2.parseFile(metaXSLTFilename)
+
+# setup the stylesheet instance
+style = libxslt.parseStylesheetDoc(styledoc)
+
+# parse the inline generated XML file
+doc = libxml2.parseDoc(meta_string)
+
+# apply the stylesheet instance to the document instance
+result = style.applyStylesheet(doc, None)
+
+result.debugDumpDocument(None)
+
+# save result to stdout "-"
+style.saveResultToFilename("-", result, 0)
+
+# free instances
+result.freeDoc()
+style.freeStylesheet()
+doc.freeDoc()
+
+
+#output = libxsltmod.translate_to_string('f', 'meta.xsl',
+#                                        's', meta_string)
+
+#print output
