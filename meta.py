25190a761567c06a5ae088a5a2768ad6334ef546
Alex Lancaster <alexl@berkeley.edu>
2002-04-16 12:00:56 +0000

    Add "-d" to simply pass the generated meta XML file directly to stdout.

--- meta.py
+++ meta.py
@@ -2,66 +2,14 @@
 import os, sys, libxsltmod
 from getopt import getopt, GetoptError
 
-usage_message = """Usage: meta.py [OPTION] INPUTFILES...
-
-Processes INPUTFILES and generates 'meta'-analyses.  INPUTFILES are
-expected to be the XML output files taken from runs of 'pypop'.
-
-  -m, --meta-xslt=FILE    use specified XSLT FILE (default: 'xslt/meta.xsl')
-  -h, --help              show this message
-
-  INPUTFILES  input XML files""" 
-
-try:
-  opts, args =getopt(sys.argv[1:],"m:h", ["meta-xslt=", "help"])
-except GetoptError:
-  sys.exit(usage_message)
-
-metaXSLTFilename= 'xslt/meta.xsl'
-
-# parse options
-for o, v in opts:
-  if o in ("-m", "--meta-xslt"):
-    metaXSLTFilename = v
-  elif o in ("-h", "--help"):
-    sys.exit(usage_message)
-
-# parse arguments
-files = args
-
-# generate a metafile XML wrapper
-
-# open doctype
-meta_string="<!DOCTYPE meta [\n"
-entities = ""
-includes = ""
-
-for f in files:
-    base = os.path.basename(f)
-    entities += "<!ENTITY %s SYSTEM \"%s\">\n" % (base, f)
-    includes += "&%s;\n" % base
-
-# put entities after doctype
-meta_string += entities
-
-# close doctype
-meta_string += "]>\n"
-
-# open tag
-meta_string += "<meta>\n"
-
-# include content
-meta_string += includes
-
-# close tag
-meta_string += "</meta>"
-
 def translate_to_stdout(xslFilename, inString):
     
     # do the transformation
     import libxml2
     import libxslt
 
+    libxslt.registerAllExtras()
+    
     # force the libxml2 processor to generate DOM trees compliant with
     # the XPath data model.
     libxml2.lineNumbersDefault(1)
@@ -96,7 +44,67 @@ def translate_to_stdout(xslFilename, inString):
 
     #return outString
 
+usage_message = """Usage: meta.py [OPTION] INPUTFILES...
 
-output = translate_to_stdout(metaXSLTFilename, meta_string)
+Processes INPUTFILES and generates 'meta'-analyses.  INPUTFILES are
+expected to be the XML output files taken from runs of 'pypop'.
+
+  -m, --meta-xslt=FILE    use specified XSLT FILE (default: 'xslt/meta.xsl')
+  -h, --help              show this message
+  -d, --dump-meta         dump the meta output file to stdout, ignore xslt file
+
+  INPUTFILES  input XML files""" 
+
+try:
+  opts, args =getopt(sys.argv[1:],"m:hd", ["meta-xslt=", "help", "dump-meta"])
+except GetoptError:
+  sys.exit(usage_message)
+
+metaXSLTFilename= 'xslt/meta.xsl'
+dump_meta = 0
+
+# parse options
+for o, v in opts:
+  if o in ("-m", "--meta-xslt"):
+    metaXSLTFilename = v
+  elif o in ("-h", "--help"):
+    sys.exit(usage_message)
+  elif o in ("-d", "--dump-meta"):
+    dump_meta = 1
+
+# parse arguments
+files = args
+
+# generate a metafile XML wrapper
+
+# open doctype
+meta_string="<!DOCTYPE meta [\n"
+entities = ""
+includes = ""
+
+for f in files:
+    base = os.path.basename(f)
+    entities += "<!ENTITY %s SYSTEM \"%s\">\n" % (base, f)
+    includes += "&%s;\n" % base
+
+# put entities after doctype
+meta_string += entities
+
+# close doctype
+meta_string += "]>\n"
+
+# open tag
+meta_string += "<meta>\n"
+
+# include content
+meta_string += includes
+
+# close tag
+meta_string += "</meta>"
+
+if dump_meta == 1:
+  print meta_string
+else:
+  output = translate_to_stdout(metaXSLTFilename, meta_string)
 
 
