dcc8710b203a49c792e08a2ee19bc4653e6161b1
Alex Lancaster <alexl@berkeley.edu>
2002-05-02 05:58:51 +0000

    Fork xsltproc process, because Python bindings don't grok exslt extensions as yet. New wrapper functions for Python bindings. Change meaning of "-m" option to specify directory of XSLT files, not the file itself.  Simply run new meta-to-r.xsl directory. Prefix all ENTITYs by "ENT" to avoid entities starting with numeric characters.

--- meta.py
+++ meta.py
@@ -2,21 +2,21 @@
 import os, sys, libxsltmod
 from getopt import getopt, GetoptError
 
-def translate_to_stdout(xslFilename, inString):
+import libxml2
+import libxslt
+
+libxslt.registerAllExtras()
     
+# force the libxml2 processor to generate DOM trees compliant with
+# the XPath data model.
+libxml2.lineNumbersDefault(1)
+
+# set libxml2 to substitute the entities in the document by default...
+libxml2.substituteEntitiesDefault(1)
+
+def _translate_string_to(xslFilename, inString, outFile):
     # do the transformation
-    import libxml2
-    import libxslt
-
-    libxslt.registerAllExtras()
     
-    # force the libxml2 processor to generate DOM trees compliant with
-    # the XPath data model.
-    libxml2.lineNumbersDefault(1)
-
-    # set libxml2 to substitute the entities in the document by default...
-    libxml2.substituteEntitiesDefault(1)
-
     # parse the stylesheet file
     styledoc = libxml2.parseFile(xslFilename)
 
@@ -29,8 +29,7 @@ def translate_to_stdout(xslFilename, inString):
     # apply the stylesheet instance to the document instance
     result = style.applyStylesheet(doc, None)
     
-    # save result to stdout "-"
-    style.saveResultToFilename("-", result, 0)
+    style.saveResultToFilename(outFile, result, 0)
 
     # use to dump directly to a string, problem is that it insists on
     # outputting an XML declaration "<?xml ...?>", can't seem to
@@ -41,15 +40,48 @@ def translate_to_stdout(xslFilename, inString):
     result.freeDoc()
     style.freeStylesheet()
     doc.freeDoc()
-
     #return outString
+    
+def translate_string_to_stdout(xslFilename, inString):
+    # save result to stdout "-"
+    _translate_string_to(xslFilename, inString, "-")
+
+def translate_string_to_file(xslFilename, inString, outFile):
+    _translate_string_to(xslFilename, inString, outFile)
+
+def _translate_file_to(xslFilename, inFile, outFile):
+    
+    # parse the stylesheet file
+    styledoc = libxml2.parseFile(xslFilename)
+
+    # setup the stylesheet instance
+    style = libxslt.parseStylesheetDoc(styledoc)
+
+    # parse the inline generated XML file
+    doc = libxml2.parseFile(inFile)
+
+    # apply the stylesheet instance to the document instance
+    result = style.applyStylesheet(doc, None)
+    
+    style.saveResultToFilename(outFile, result, 0)
+
+    # free instances
+    result.freeDoc()
+    style.freeStylesheet()
+    doc.freeDoc()
+
+def translate_file_to_stdout(xslFilename, inFile):
+    _translate_file_to(xslFilename, inFile, "-")
+
+def translate_file_to_file(xslFilename, inFile, outFile):
+    _translate_file_to(xslFilename, inFile, outFile)
 
 usage_message = """Usage: meta.py [OPTION] INPUTFILES...
 
 Processes INPUTFILES and generates 'meta'-analyses.  INPUTFILES are
 expected to be the XML output files taken from runs of 'pypop'.
 
-  -m, --meta-xslt=FILE    use specified XSLT FILE (default: 'xslt/meta.xsl')
+  -m, --meta-xslt=DIR     use specified directory to find XSLT (default: 'xslt')
   -h, --help              show this message
   -d, --dump-meta         dump the meta output file to stdout, ignore xslt file
 
@@ -60,13 +92,13 @@ try:
 except GetoptError:
   sys.exit(usage_message)
 
-metaXSLTFilename= 'xslt/meta.xsl'
+metaXSLTDirectory= 'xslt/meta.xsl'
 dump_meta = 0
 
 # parse options
 for o, v in opts:
   if o in ("-m", "--meta-xslt"):
-    metaXSLTFilename = v
+    metaXSLTDirectory = v
   elif o in ("-h", "--help"):
     sys.exit(usage_message)
   elif o in ("-d", "--dump-meta"):
@@ -84,8 +116,9 @@ includes = ""
 
 for f in files:
     base = os.path.basename(f)
-    entities += "<!ENTITY %s SYSTEM \"%s\">\n" % (base, f)
-    includes += "&%s;\n" % base
+    ent = "ENT" + base
+    entities += "<!ENTITY %s SYSTEM \"%s\">\n" % (ent, f)
+    includes += "&%s;\n" % ent
 
 # put entities after doctype
 meta_string += entities
@@ -103,8 +136,14 @@ meta_string += includes
 meta_string += "</meta>"
 
 if dump_meta == 1:
-  print meta_string
+    print meta_string
 else:
-  output = translate_to_stdout(metaXSLTFilename, meta_string)
-
-
+    f = open('meta.xml', 'w')
+    f.write(meta_string)
+    f.close()
+    #translate_string_to_file(os.path.join(metaXSLTDirectory, 'sort-by-locus.xsl'), meta_string, 'sorted.xml')
+    #translate_file_to_stdout(os.path.join(metaXSLTDirectory, 'meta-to-r.xsl'), 'sorted.xml')
+    # have to resort to command-line version because the Python bindings
+    # don't understand how to load the exslt extensions in libxslt yet
+    # that the meta-to-r.xsl stylesheet uses
+    os.popen("xsltproc %s %s" % (os.path.join(metaXSLTDirectory, 'meta-to-r.xsl'), 'meta.xml'))
