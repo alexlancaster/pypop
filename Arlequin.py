2ca783379ee55879ab6ef263cdbe8a85dff7b663
alex <>
2002-05-09 00:23:23 +0000

    Formatting tweak.

--- Arlequin.py
+++ Arlequin.py
@@ -311,8 +311,8 @@ genetics program.
  -i, --idcol=NUM       column number of identifier (first column is zero)
  -l, --ignorelines=NUM number of header lines to ignore in in file
  -c, --cols=POS1,POS2  number of leading columns (POS1) before start and
-                       number of trailing colums before the end (POS2) of
-                       allele data (including IDCOL)
+                        number of trailing colums before the end (POS2) of
+                        allele data (including IDCOL)
  -k, --sort=POS1,..    specify order of loci if different from column order
                         in file (must not repeat a locus)
  -w, --windowsize=NUM  number of loci involved in window size 
