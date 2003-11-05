bb38594512f1b86d7529d8ae20f0b4a946580e2b
alex <>
2003-11-05 10:58:18 +0000

    This is a script to do PyPop-build sanity check for both binary and standalones.  You can choose to build a real package, and CVS will be checked to make sure it is up-to-date, if it is, and only if it is the source will then be tagged via CVS with version number.  If you don't build real package '-dummy' will be automatically appended to version number, and CVS won't be checked.

---
+++ check-status-tag.sh
@@ -0,0 +1,42 @@
+#/bin/sh
+echo "Welcome to the PyPop-build sanity check"
+echo
+echo "* if you build a real package CVS will be checked to make sure it is up-to-date"
+echo "  and the source will be tagged via CVS with version number"
+echo "* if you don't build real package '-dummy' will be appended to version "
+echo
+echo -n "do you want to build a real package? enter y: "
+
+read REAL
+
+if test "x$REAL" = xy; then
+    echo "running cvs status on directory tree..."
+    CHK=$(cvs -Q status|grep "Status: "|grep -v "Up-to-date")
+    if test "x$CHK" != x; then
+	echo $CHK
+	echo
+	echo "your module is not up-to-date, you must make sure all source is checked in"
+	echo "(using 'cvs commit') and is also up-to-date (use 'cvs update')"
+	echo "automatically switching to 'dummy' mode"
+	echo
+	DONTTAG=y
+	EXTRA=-dummy
+    fi
+else
+    DONTTAG=y
+    EXTRA=-dummy
+fi
+
+echo -n "Enter the VERSION tag (only alphanumeric, '.', '_' or '-' allowed): "
+read VERSION
+VERSION=${VERSION}${EXTRA}
+echo "VERSION is $VERSION"
+echo $VERSION > VERSION
+
+if test x$DONTTAG = xy; then
+    echo "skipping tagging"
+else
+    CVSTAG=PYPOP_SRC-$(cat VERSION|tr . _)
+    echo cvs tag $CVSTAG
+fi
+
