6127c554f7854c6b494b234371cb636efa6d1ce6
rsingle <richard.single@uvm.edu>
2003-12-03 20:12:41 +0000

    increased MAX_ALLELES to 100 from 80 for a large dataset from Marcelo   MAX_INIT set to 50
      added line below in main():
        fprintf(fp_out, "Sample Size (n): %d\n", num_recs);
    
      added count_permu and checking error_flag for permus before counting as a valid permu
        see // RS 20031125
    
      removed a section from emcalc that was commented out

--- emhaplofreq/emhaplofreq.h
+++ emhaplofreq/emhaplofreq.h
@@ -35,7 +35,7 @@ MODIFICATIONS. */
 #define NAME_LEN    12       /* 10 chars for allele name, plus colon and null */ 
 #define LINE_LEN    132      /* RS changed from 120 to 132=6*2*(10+1) */ 
 #define MAX_ROWS    1023      
-#define MAX_ALLELES 80 
+#define MAX_ALLELES 100      /* increased from 80 for a large dataset */
 #define MAX_LOCI    7 
 #define MAX_COLS    MAX_LOCI * 2 
                              /* max genotypes:  2^max_loci*max_rows */ 
@@ -48,7 +48,7 @@ MODIFICATIONS. */
 #define FALSE 0 
 #define TRUE  1 
  
-#define MAX_INIT 20 
+#define MAX_INIT 50 
 
 #define MAX_GENOS_PER_PHENO 64 /* 2^(max_loci - 1) */
 
