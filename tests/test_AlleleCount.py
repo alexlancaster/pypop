8ee2db901bfb84841b01157892361cc2791bd47b
Alex Lancaster <alancaster@amberbiology.com>
2017-06-21 00:19:48 -0400

    tidy

--- tests/test_AlleleCount.py
+++ tests/test_AlleleCount.py
@@ -3,15 +3,16 @@ import hashlib
 import pytest
 
 def test_AlleleColon():
-    try:
-        retcode=subprocess.Popen(
-            ['./bin/pypop.py', '-m', '-c', './data/ihiws2017/Test_Allele_Colon_HardyWeinberg.ini', './data/ihiws2017/Test_Allele_Colon_HardyWeinberg.pop'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
-            )
-    except:
-        print "failed to run!"
-    retcode.wait()  # wait until script completed
+    process=subprocess.Popen(
+        ['./bin/pypop.py', '-m', '-c', './data/ihiws2017/Test_Allele_Colon_HardyWeinberg.ini', './data/ihiws2017/Test_Allele_Colon_HardyWeinberg.pop'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+        )
+    process.communicate()
+    exit_code = process.wait()  # wait until script completed
+
+    # check exit code
+    assert exit_code == 0
     # compare with md5sum of output file
     assert hashlib.md5(open("Test_Allele_Colon_HardyWeinberg-out.txt", 'rb').read()).hexdigest() == 'ba0cd73c8ccd28fed726b0825688d620'
 
