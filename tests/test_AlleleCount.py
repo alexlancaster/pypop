49e5d7c8383d7ce9b8349d9be679c6b1b2d99e97
Alex Lancaster <alancaster@amberbiology.com>
2017-06-21 17:57:38 -0400

    Resync the md5 with the new output.

--- tests/test_AlleleCount.py
+++ tests/test_AlleleCount.py
@@ -14,5 +14,5 @@ def test_AlleleColon():
     # check exit code
     assert exit_code == 0
     # compare with md5sum of output file
-    assert hashlib.md5(open("Test_Allele_Colon_HardyWeinberg-out.txt", 'rb').read()).hexdigest() == '59c5f520b7700ac4fde03aac93640460'
+    assert hashlib.md5(open("Test_Allele_Colon_HardyWeinberg-out.txt", 'rb').read()).hexdigest() == '86b09eb23ac6a17eaa6aaa498b552e3f'
 
