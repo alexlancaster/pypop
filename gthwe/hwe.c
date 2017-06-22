49a36b9c6757a740754b0f4f0354ffb1e5e74cef
Alex Lancaster <alancaster@amberbiology.com>
2017-06-22 00:50:25 -0400

    Revert "If testing mode is set when doing MCMC, set random number seed used by GSL to be fixed to get deterministic output."
    This reverts commit 549c05890be99e21bd0136d8bd56e38c2a2676bc.

--- gthwe/hwe.c
+++ gthwe/hwe.c
@@ -404,19 +404,10 @@ int run_randomization(int *genotypes, int *allele_array, int no_allele,
 
   const gsl_rng_type * T;
   gsl_rng * r;
-  long seed;
   
   gsl_rng_env_setup();
   T = gsl_rng_default;
-  
-  if (testing) { 
-    seed = 1234; // fix seed if in testing mode
-  }  
-  else {
-    seed = time(NULL);
-  }
-  r = gsl_rng_alloc(T);
-  gsl_rng_set(r, seed);  // set seed
+  r = gsl_rng_alloc (T);
 
   /* create empty genotype array */
   int *g = (int *)calloc(num_genotypes, sizeof(int));
