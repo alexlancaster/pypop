9d0586f461b1995cd81ae5f9f52816049968f630
Alex Lancaster <alancaster@amberbiology.com>
2017-07-14 23:01:14 -0400

    Update tests to make test suite work again after many output changes (#4 #17 #18)

--- tests/test_GenotypeCommon.py
+++ tests/test_GenotypeCommon.py
@@ -8,11 +8,11 @@ def test_GenotypeCommon_HardyWeinberg():
     # check exit code
     assert exit_code == 0
     # compare with md5sum of output file
-    assert hashlib.md5(open("BIGDAWG_SynthControl_Data-out.txt", 'rb').read()).hexdigest() == '276263b0d0d9fc03b77826388d70510d'
+    assert hashlib.md5(open("BIGDAWG_SynthControl_Data-out.txt", 'rb').read()).hexdigest() == 'c0f35b5b30ed20211fd37ff0a6021061'
 
 def test_GenotypeCommonDash_HardyWeinberg():
     exit_code = base.run_pypop_process('./tests/data/WS_BDCtrl_Test_HW.ini', './tests/data/BIGDAWG_SynthControl_Data_dash.pop')
     # check exit code
     assert exit_code == 0
     # compare with md5sum of output file
-    assert hashlib.md5(open("BIGDAWG_SynthControl_Data_dash-out.txt", 'rb').read()).hexdigest() == 'b0f4247a2a67a65d0109b6448427cc28'
+    assert hashlib.md5(open("BIGDAWG_SynthControl_Data_dash-out.txt", 'rb').read()).hexdigest() == '7dce2213b3ec0109ed232b40d71ad961'
