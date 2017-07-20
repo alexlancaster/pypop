5abd01f6e9ba88c94de43115b26768b20f806c36
Alex Lancaster <alancaster@amberbiology.com>
2017-07-20 17:15:49 -0400

    note that file comparison is not yet implemented.

--- tests/test_GenerateTSV.py
+++ tests/test_GenerateTSV.py
@@ -7,5 +7,5 @@ def test_GenerateTSV():
     exit_code = base.run_pypop_process('./tests/data/WS_BDCtrl_Test_HW.ini', './tests/data/BIGDAWG_SynthControl_Data.pop', args=['--generate-tsv'])
     # check exit code
     assert exit_code == 0
-    # compare with md5sum of output file
-    #assert hashlib.md5(open("USAFEL-UchiTelle-small-out.txt", 'rb').read()).hexdigest() == '97f06318478f8faa6b4a17397ca0edfe'
+    # compare with output files
+    # FIXME: not yet implemented
