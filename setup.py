18f4e7b99f7ff40de590c04c9ec4f7e475f65e3b
Alex Lancaster <alancaster@amberbiology.com>
2017-07-26 00:59:33 -0400

    require psutil for tests.

--- setup.py
+++ setup.py
@@ -211,5 +211,5 @@ particularly large-scale multilocus genotype data""",
        ext_modules=extensions,
        cmdclass={'clean': CleanCommand,},
        setup_requires=['pytest-runner'],
-       tests_require=['pytest'],
+       tests_require=['pytest', 'psutil'],
        )
