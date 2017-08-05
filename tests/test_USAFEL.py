e24add29538901524781d96939ce0df5c5b9ad57
Alex Lancaster <alancaster@amberbiology.com>
2017-08-05 00:59:03 -0400

    output the haplotypes (using lociToEstHaplo=* in .ini file) convert test to compare file output

--- tests/test_USAFEL.py
+++ tests/test_USAFEL.py
@@ -2,10 +2,14 @@ import base
 import subprocess
 import hashlib
 import pytest
+import os.path
+import filecmp
 
 def test_USAFEL():
     exit_code = base.run_pypop_process('./tests/data/minimal.ini', './tests/data/USAFEL-UchiTelle-small.pop')
     # check exit code
     assert exit_code == 0
-    # compare with md5sum of output file
-    assert hashlib.md5(open("USAFEL-UchiTelle-small-out.txt", 'rb').read()).hexdigest() == '97f06318478f8faa6b4a17397ca0edfe'
+
+    out_filename = "USAFEL-UchiTelle-small-out.txt"
+    gold_out_filename = os.path.join('./tests/data/output', out_filename)
+    assert filecmp.cmp(out_filename, gold_out_filename)
