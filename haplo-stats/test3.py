fdccfddbca8ed404669411f2b097d0fe81b69401
rsingle <rsingle@uvm.edu>
2017-08-09 07:47:38 -0700

    fixed assignments for dprime_den (removed unnecessary loop in first attempt)

--- haplo-stats/test3.py
+++ haplo-stats/test3.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import numpy
 
+# NB: haplotype[i, j] contains allele for ith haplo at jth locus 
 # two locus haplotypes array
 haplotype = numpy.array([['A1', 'B1'], ['A2', 'B1'], ['A1', 'B2'], ['A2', 'B2']],dtype='O')
 
 # get "shape" of array, in this case, 2d, so rows and cols as a "tuple"
 rows, cols = haplotype.shape
 
-#NB: haplotype[i, j] contains allele for ith haplo at jth locus 
-
 for j in range(0, cols):
     for i in range(0, rows):
         print i, j, haplotype[i, j]
 
+# print haplo table with 1 row per haplo
 print "NB: haplotype[i, j] contains allele for ith haplo at jth locus"
 print "i haplo"
 for i in range(0, rows):
@@ -32,19 +32,10 @@ dprime_den = zero.repeat(rows)
 d_ij = hap_prob - a_freq1 * a_freq2
 den_lt0 = numpy.minimum( a_freq1*a_freq2, (1-a_freq1)*(1-a_freq2) )
 den_ge0 = numpy.minimum( (1-a_freq1)*a_freq2, a_freq1*(1-a_freq2) )
-#FIXME: LOOP NOT NEEDED HERE
-for i in range(0,rows): 
-    if d_ij[i]<0.0: 
-       dprime_den[i]=den_lt0[i] 
-    else:
-       dprime_den[i]=den_ge0[i] 
-  #WANTED TO USE SOMETHING LIKE THIS:
-  # dprime_den[d_ij < 0] <- den_lt0[d_ij < 0]
-  # dprime_den[d_ij >=0] <- den_ge0[d_ij >=0]
-  #TRIED (BUT DIDN'T WORK) I THINK I NEED A 'numpy.' SOMEWHERE
-  # dprime_den = d_ij
-  # (dprime_den<0).choose(den_ge0,den_lt0)
+dprime_den[d_ij < 0] = den_lt0[d_ij < 0]
+dprime_den[d_ij >=0] = den_ge0[d_ij >=0]
 dprime_ij = d_ij/dprime_den
+print "dprime_den:", dprime_den
 
 print "i a_freq1 a_freq2 d_ij dprime hap_prob haplo"
 for i in range(0, rows):
@@ -53,10 +44,10 @@ for i in range(0, rows):
         print haplotype[i, j],
     print
 
-print "alleles1:", alleles1 , "CURRENTLY HARD CODED"
-print "alleles2:", alleles2
-print "length alleles1:", numpy.unique(alleles1).size*1.0 , "MAY BE A BETTER WAY TO GET"
-print "length alleles2:", numpy.unique(alleles2).size*1.0
+print "  alleles1:", alleles1 , "CURRENTLY HARD CODED"
+print "  alleles2:", alleles2
+print "  length(alleles1):", numpy.unique(alleles1).size*1.0 , "MAY BE A BETTER WAY TO GET"
+print "  length(alleles2):", numpy.unique(alleles2).size*1.0
 
 dp_temp = abs(dprime_ij)*a_freq1*a_freq2
 dprime = dp_temp.sum()
