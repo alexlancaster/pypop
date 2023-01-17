b08ac82e18930c765c4b16d33bfa076282489df8
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-01-16 23:16:39 -0500

    update unit tests to track with changes to emhaplofreq text output
    Fixes #44, #53

--- tests/test_AlleleColon.py
+++ tests/test_AlleleColon.py
@@ -16,4 +16,4 @@ def test_AlleleColon_Emhaplofreq():
     # check exit code
     assert exit_code == 0
     # compare with md5sum of output file
-    assert hashlib.md5(open("Test_Allele_Colon_Emhaplofreq-out.txt", 'rb').read()).hexdigest() == '598954bfe301d5dea44ccd4d443905d1'
+    assert hashlib.md5(open("Test_Allele_Colon_Emhaplofreq-out.txt", 'rb').read()).hexdigest() == '0434be553e01a165994190107fa48035'
