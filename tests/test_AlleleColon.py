14b5cd62a7944d1fc16efe1cbe1f9516b10c5449
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-01-31 16:50:05 -0500

    update tests wtih expected fails (XFAIL) and skipped
    add a USAFEL test that doesn't call GuoThompson, to test pipeline
    disable other tests, will reenable when module is fixed

--- tests/test_AlleleColon.py
+++ tests/test_AlleleColon.py
@@ -3,6 +3,7 @@ import subprocess
 import hashlib
 import pytest
 
+@pytest.mark.xfail(reason="HardyWeinbergGuoThompson module currently broken")
 def test_AlleleColon_HardyWeinberg():
     exit_code = base.run_pypop_process('./tests/data/Test_Allele_Colon_HardyWeinberg.ini', './tests/data/Test_Allele_Colon_HardyWeinberg.pop')
     # check exit code
@@ -10,7 +11,7 @@ def test_AlleleColon_HardyWeinberg():
     # compare with md5sum of output file
     assert hashlib.md5(open("Test_Allele_Colon_HardyWeinberg-out.txt", 'rb').read()).hexdigest() == '245a8a8493506c0b65ba9a3469173b13'
 
-@pytest.mark.skip(reason="emhaplofreq not functional in Python 3")
+@pytest.mark.xfail(reason="Emhaplofreq module not functional in Python 3")
 def test_AlleleColon_Emhaplofreq():
     exit_code = base.run_pypop_process('./tests/data/Test_Allele_Colon_Emhaplofreq.ini', './tests/data/Test_Allele_Colon_Emhaplofreq.pop')
     # check exit code
--- tests/test_USAFEL.py
+++ tests/test_USAFEL.py
@@ -6,6 +6,17 @@ import os.path
 import filecmp
 
 def test_USAFEL():
+    exit_code = base.run_pypop_process('./tests/data/minimal-no-emhaplofreq-no-guothompson.ini', './tests/data/USAFEL-UchiTelle-small.pop')
+    # check exit code
+    assert exit_code == 0
+
+    out_filename = "USAFEL-UchiTelle-small-out.txt"
+    gold_out_filename = base.abspath_test_data(os.path.join('./tests/data/output', "USAFEL-UchiTelle-small-out-no-emhaplofreq-noguothompson.txt"))
+    assert filecmp.cmp(out_filename, gold_out_filename)
+
+
+@pytest.mark.xfail(reason="GuoThompson not yet functional in Python 3")
+def test_USAFEL_guothompson():
     exit_code = base.run_pypop_process('./tests/data/minimal-no-emhaplofreq.ini', './tests/data/USAFEL-UchiTelle-small.pop')
     # check exit code
     assert exit_code == 0
@@ -14,7 +25,7 @@ def test_USAFEL():
     gold_out_filename = base.abspath_test_data(os.path.join('./tests/data/output', "USAFEL-UchiTelle-small-out-no-emhaplofreq.txt"))
     assert filecmp.cmp(out_filename, gold_out_filename)
 
-@pytest.mark.skip(reason="emhaplofreq not functional in Python 3")
+@pytest.mark.xfail(reason="Emhaplofreq not functional in Python 3")
 def test_USAFEL_emhaplofreq():
     exit_code = base.run_pypop_process('./tests/data/minimal.ini', './tests/data/USAFEL-UchiTelle-small.pop')
     # check exit code
