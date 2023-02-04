14e06460206c3a2f1041394571293a47123898e9
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-02-04 00:32:26 -0500

    port to Python 3
    update call to estHaplotypes()

--- haplo-stats/test1.py
+++ haplo-stats/test1.py
@@ -55,37 +55,37 @@ control = {'max_iter': 5000,
 # FIXME: currently this assumes that geno StringMatrix contains only the loci required
 # need to make sure that this works with subMatrices
 
-import StringIO
+import io
 from PyPop.Utils import XMLOutputStream
 
-xmlOutput = XMLOutputStream(StringIO.StringIO())
+xmlOutput = XMLOutputStream(io.StringIO())
 
 haplo = Haplostats(geno, stream=xmlOutput)
-converge, lnlike, n_u_hap, n_hap_pairs, hap_prob, u_hap, u_hap_code, subj_id, post, hap1_code, hap2_code, haplotype = \
+converge, lnlike, n_u_hap, n_hap_pairs, hap_prob, u_hap, u_hap_code, subj_id, post, hap1_code, hap2_code, haplotype, dprime, Wn, ALD_1_2, ALD_2_1 = \
           haplo.estHaplotypes(weight=None, control=control, numInitCond=1)
 
-print " converge:", converge
-print " lnlike:", lnlike
-print " n_u_hap:", n_u_hap
-print " n_hap_pairs:", n_hap_pairs
-print " hap_prob:", hap_prob
-print " u_hap:", u_hap
-print " u_hap_code:", u_hap_code
-print " subj_id:", subj_id
-print " post:", post
-print " hap1_code:", hap1_code
-print " hap2_code:", hap2_code
+print(" converge:", converge)
+print(" lnlike:", lnlike)
+print(" n_u_hap:", n_u_hap)
+print(" n_hap_pairs:", n_hap_pairs)
+print(" hap_prob:", hap_prob)
+print(" u_hap:", u_hap)
+print(" u_hap_code:", u_hap_code)
+print(" subj_id:", subj_id)
+print(" post:", post)
+print(" hap1_code:", hap1_code)
+print(" hap2_code:", hap2_code)
 
 # Print columns side-by-side for easier checking
 # NB: u_hap is trickier since it has n.loci entries per haplo
-print 'hap_prob  u_hap_code u_hap(needs to be split for printing)'
-print numpy.c_[hap_prob,u_hap_code]
-print 'subj_id  hap1_code  hap2_code'
-print numpy.c_[subj_id,hap1_code,hap2_code]
+print('hap_prob  u_hap_code u_hap(needs to be split for printing)')
+print(numpy.c_[hap_prob,u_hap_code])
+print('subj_id  hap1_code  hap2_code')
+print(numpy.c_[subj_id,hap1_code,hap2_code])
 #   for x1,x2,x3 in zip(hap_prob,u_hap,u_hap_code):
 #       print x1 + '\t\t' + x2 + '\t\t' + x3
 
 
-print "sample XML output"
-print xmlOutput.f.getvalue()
+print("sample XML output")
+print(xmlOutput.f.getvalue())
 
