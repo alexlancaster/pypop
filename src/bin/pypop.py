5f4516db35e7fdf4847df744fdc85cfd42cbcf8f
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-06-06 19:17:27 -0400

    replace old `getopt` with `argparse` library (#66)
    remove obsolete options:
    * -l, --use-libxslt / -s, --use-4suite: 4suite no longer maintained, use
      lxml only
    * -g, --gui: wxpython library no longer maintained
    other tidying up

--- src/bin/pypop.py
+++ src/bin/pypop.py
@@ -42,7 +42,6 @@ DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(DIR, '..'))
 sys.path.insert(0, os.path.join(DIR, '../src'))
 
-
 import PyPop
 
 ######################################################################
@@ -71,34 +70,10 @@ pkgname = PyPop.__pkgname__
 # BEGIN: generate message texts
 ######################################################################
 
-copyright_message = """Copyright (C) 2003-2005 Regents of the University of California
+copyright_message = """Copyright (C) 2003-2006 Regents of the University of California.
+Copyright (C) 2007-2023 PyPop team.
 This is free software.  There is NO warranty; not even for
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."""
-
-usage_message = """Usage: pypop [OPTION]... [INPUTFILE]...
-Process and run population genetics statistics on one or more INPUTFILEs.
-Expects to find a configuration file called 'config.ini' in the
-current directory or in %s.
-
-  -l, --use-libxslt    filter XML via XSLT using libxslt (default)
-  -s, --use-4suite     filter XML via XSLT using 4Suite
-  -x, --xsl=FILE       use XSLT translation file FILE
-  -h, --help           show this message
-  -c, --config=FILE    select alternative config file
-  -d, --debug          enable debugging output (overrides config file setting)
-  -i, --interactive    run in interactive mode, prompting user for file names
-  -g, --gui            run GUI (currently disabled)
-  -o, --outputdir=DIR  put output in directory DIR
-  -f, --filelist=FILE  file containing list of files (one per line) to process
-                        (mutually exclusive with supplying INPUTFILEs)
-      --generate-tsv   generate TSV output files (aka run 'popmeta')
-  -V, --version        print version of PyPop
-  -m, --testmode       run PyPop in test mode for unit testing
-  
-  INPUTFILE   input text file""" % altpath
-
-version_message = """pypop %s
-%s""" % (version, copyright_message)
 
 interactive_message = """PyPop: Python for Population Genomics (%s)
 %s
@@ -119,70 +94,54 @@ return for each prompt.
 # BEGIN: parse command line options
 ######################################################################
 
-from getopt import getopt, GetoptError
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
+from pathlib import Path
 from glob import glob
 from configparser import ConfigParser
 from PyPop.Main import getUserFilenameInput, checkXSLFile
 
-try:
-  opts, args =getopt(sys.argv[1:],"mlsigc:hdx:f:o:V", ["testmode", "use-libxslt", "use-4suite", "interactive", "gui", "config=", "help", "debug", "xsl=", "filelist=", "outputdir=", "version", "generate-tsv"])
-except GetoptError:
-  sys.exit(usage_message)
 
-# default options
-use_libxsltmod = 0
-use_FourSuite = 0
-configFilename = 'config.ini'
-specifiedConfigFile = 0
-debugFlag = 0
-interactiveFlag = 0
-guiFlag = 0
-xslFilename = None
-outputDir = None
-fileList = None
-generateTSV = 0
-testMode = False
+parser = ArgumentParser(prog="pypop.py", description="""Process and run population genetics statistics on one or more INPUTFILEs.\n
+Expects to find a configuration file called 'config.ini' in the\n
+current directory""", epilog=copyright_message, formatter_class=ArgumentDefaultsHelpFormatter)
 
-# parse options
-for o, v in opts:
-  if o in ("-l", "--use-libxslt"):
-    use_libxsltmod = 1
-  elif o in ("-s", "--use-4suite"):
-    use_FourSuite = 1
-  elif o in ("-c", "--config"):
-    configFilename = v
-    specifiedConfigFile = 1
-  elif o in ("-x", "--xsl"):
-    xslFilename = v
-  elif o in ("-d", "--debug"):
-    debugFlag = 1
-  elif o in ("-h", "--help"):
-    sys.exit(usage_message)
-  elif o in ("-i", "--interactive"):
-    interactiveFlag = 1
-  elif o in ("-g", "--gui"):
-    guiFlag = 1
-  elif o in ("--generate-tsv"):
-    generateTSV = 1
-  elif o in ("-f", "--filelist"):
-    if os.path.isfile(v):
-      fileList = v
-    else:
-      sys.exit("'%s' is not a file, please supply a valid file" % v)
-  elif o in ("-o", "--outputdir"):
-    if os.path.isdir(v):
-      outputDir = v
-    else:
-      sys.exit("'%s' is not a directory, please supply a valid output directory" % v)
-  elif o in ("-V", "--version"):
-    sys.exit(version_message)
-  elif o in ("-m", "--testmode"):
-    testMode = True
+parser.add_argument("-c", "--config", help="select config file",
+                    required=False, default='config.ini')
+parser.add_argument("-m", "--testmode", help="run PyPop in test mode for unit testing", action='store_true', required=False, default=False)
+parser.add_argument("-d", "--debug", help="enable debugging output (overrides config file setting)",
+                    action='store_true', required=False, default=False)
+parser.add_argument("-t", "--generate-tsv", help="generate TSV output files (aka run 'popmeta')",
+                    action='store_true', required=False, default=False)
+parser.add_argument("-x", "--xsl", help="override the default XSLT translation with XSLFILE", 
+                    metavar="XSLFILE", required=False, default=None)
+parser.add_argument("-o", "--outputdir", help="put output in directory DIR",
+                    required=False, type=Path, default=None)
+parser.add_argument("-V", "--version", action='version', version="%(prog)s {version} {copyright}".format(version=version, copyright=copyright_message))
+
+gp = parser.add_argument_group('Mutually exclusive input options')
+gpm = gp.add_mutually_exclusive_group(required=True)
+gpm.add_argument("-i", "--interactive", help="run in interactive mode, prompting user for file names",
+                 action='store_true', default=False)
+gpm.add_argument("-f", "--filelist", help="file containing list of files (one per line) to process\n(mutually exclusive with supplying POPFILEs)",
+                 type=FileType('r'), default=None)
+gpm.add_argument("popfiles", metavar="POPFILE", help="input population ('.pop') file(s)", nargs='*', default=[])
+
+args = parser.parse_args()
+                    
+configFilename = args.config
+xslFilename = args.xsl
+debugFlag = args.debug
+interactiveFlag = args.interactive
+generateTSV = args.generate_tsv
+testMode = args.testmode
+fileList = args.filelist
+outputDir = args.outputdir
+popFilenames = args.popfiles      
+
+if outputDir:
+    if not outputDir.is_dir():
+      sys.exit("'%s' is not a directory, please supply a valid output directory" % outputDir)
     
-# if neither option is set explicitly, use libxslt python wrappers
-if not (use_libxsltmod or use_FourSuite):
-  use_libxsltmod = 1
-
 # heuristics for default 'text.xsl' XML -> text file
 
 if xslFilename:
@@ -222,152 +181,133 @@ else:
       xslFilenameDefault = checkXSLFile('text.xsl', pypopbinpath, \
                                         '../PyPop/xslt', debug=debugFlag)
 
-      ######################################################################
+######################################################################
 # END: parse command line options
 ######################################################################
 
-if guiFlag:
-  # instantiate PyPop GUI
+# call as a command-line application
 
-  if 1:
-    sys.exit("GUI support is currently disabled... sorry")
-  else:
-    #from wxPython.wx import wxPySimpleApp
-    #from GUIApp import MainWindow
+# start by assuming an empty list of filenames
+fileNames = []
+
+if interactiveFlag:
+  # run in interactive mode, requesting input from user
+
+  # Choices made in previous runs of PyPop will be stored in a file
+  # called '.pypoprc', stored the user's home directory
+  # (i.e. $HOME/.pypoprc) so that in subsequent invocations of the
+  # script it will use the previous choices as defaults.
+
+  # For systems without a concept of a $HOME directory (i.e.
+  # Windows), it will look for .pypoprc in the current directory.
+
+  # The '.pypoprc' file will be created if it does not previously
+  # exist.  The format of this file is identical to the ConfigParser
+  # format (i.e. the .ini file format).
   
-    app = wxPySimpleApp()
-    frame = MainWindow(None, -1, "PyPop",
-                       datapath = datapath,
-                       altpath = altpath,
-                       debugFlag = debugFlag)
-    app.MainLoop()
+  if os.environ['HOME']:
+    pypoprcFilename = os.path.join(os.environ['HOME'],'.pypoprc')
+  else:
+    pypoprcFilename = '.pypoprc'
 
-else:
-  # call as a command-line application
-
-  # start by assuming an empty list of filenames
-  fileNames = []
-
-  if interactiveFlag:
-    # run in interactive mode, requesting input from user
-
-    # Choices made in previous runs of PyPop will be stored in a file
-    # called '.pypoprc', stored the user's home directory
-    # (i.e. $HOME/.pypoprc) so that in subsequent invocations of the
-    # script it will use the previous choices as defaults.
-
-    # For systems without a concept of a $HOME directory (i.e.
-    # Windows), it will look for .pypoprc in the current directory.
-
-    # The '.pypoprc' file will be created if it does not previously
-    # exist.  The format of this file is identical to the ConfigParser
-    # format (i.e. the .ini file format).
+  pypoprc = ConfigParser()
     
-    if os.environ['HOME']:
-      pypoprcFilename = os.path.join(os.environ['HOME'],'.pypoprc')
+  if os.path.isfile(pypoprcFilename):
+    pypoprc.read(pypoprcFilename)
+    configFilename = pypoprc.get('Files', 'config')
+    fileName = pypoprc.get('Files', 'pop')
+  else:
+    configFilename = 'config.ini'
+    fileName = 'no default'
+
+  print(interactive_message)
+  
+  # read user input for both filenames
+  configFilename = getUserFilenameInput("config", configFilename)
+  fileNames.append(getUserFilenameInput("population", fileName))
+
+  print("PyPop is processing %s ..." % fileNames[0])
+  
+else:   
+  # non-interactive mode: run in 'batch' mode
+
+  if fileList:
+    # if we are providing the filelist
+    # use list from file as list to check
+    #li = [f.strip('\n') for f in open(fileList).readlines()]
+    li = [f.strip('\n') for f in fileList.readlines()]
+    fileList.close() # make sure we close it
+  elif popFilenames:
+    # check number of arguments, must be at least one, but can be more
+    # use args as list to check
+    #li = args
+    li = popFilenames
+  # otherwise bail out with error
+  #else:
+
+  # loop through all arguments in li, appending to list of files to
+  # process, ensuring we expand any Unix-shell globbing-style
+  # arguments
+  for fileName in li:
+    globbedFiles = glob(fileName)
+    if len(globbedFiles) == 0:
+      # if no files were found for that glob, please exit and warn
+      # the user
+      sys.exit("Couldn't find file(s): %s" % fileName)
     else:
-      pypoprcFilename = '.pypoprc'
+      fileNames.extend(globbedFiles)
 
-    pypoprc = ConfigParser()
-      
-    if os.path.isfile(pypoprcFilename):
-      pypoprc.read(pypoprcFilename)
-      configFilename = pypoprc.get('Files', 'config')
-      fileName = pypoprc.get('Files', 'pop')
-    else:
-      configFilename = 'config.ini'
-      fileName = 'no default'
+# parse config file
+from PyPop.Main import Main, getConfigInstance
+config = getConfigInstance(configFilename, altpath)
 
-    print(interactive_message)
+xmlOutPaths = []
+txtOutPaths = []
+# loop through list of filenames passed, processing each in turn
+for fileName in fileNames:
+
+  # parse out the parts of the filename
+  #baseFileName = os.path.basename(fileName)
+
+  application = Main(config=config,
+                     debugFlag=debugFlag,
+                     fileName=fileName,
+                     datapath=datapath,
+                     xslFilename=xslFilename,
+                     xslFilenameDefault=xslFilenameDefault,
+                     outputDir=outputDir,
+                     version=version,
+                     testMode=testMode)
+
+  xmlOutPaths.append(application.getXmlOutPath())
+  txtOutPaths.append(application.getTxtOutPath())
+
+if generateTSV:
+  from PyPop.Meta import Meta
+  
+  print("Generating TSV (.dat) files...")
+  Meta(popmetabinpath=pypopbinpath,
+       datapath=datapath,
+       metaXSLTDirectory=None,
+       dump_meta=0,
+       R_output=1,
+       PHYLIP_output=0,
+       ihwg_output=1,
+       batchsize=len(xmlOutPaths),
+       files=xmlOutPaths)
+
+if interactiveFlag:
+
+  print("PyPop run complete!")
+  print("XML output(s) can be found in: ",  xmlOutPaths)
+  print("Plain text output(s) can be found in: ",  txtOutPaths)
+
+  # update .pypoprc file
+
+  if pypoprc.has_section('Files') != 1:
+    pypoprc.add_section('Files')
     
-    # read user input for both filenames
-    configFilename = getUserFilenameInput("config", configFilename)
-    fileNames.append(getUserFilenameInput("population", fileName))
-
-    print("PyPop is processing %s ..." % fileNames[0])
-    
-  else:   
-    # non-interactive mode: run in 'batch' mode
-
-    if fileList and len(args) > 0:
-      sys.exit("--filelist (-f) option is mutually exclusive with INPUTFILE")
-    elif fileList:
-      # if we are providing the filelist, don't check number of args
-      # use list from file as list to check
-      li = [f.strip('\n') for f in open(fileList).readlines()]
-    elif len(args) > 0:
-      # check number of arguments, must be at least one, but can be more
-      # use args as list to check
-      li = args
-    # otherwise bail out with error
-    else:
-      sys.exit(usage_message)
-
-    # loop through all arguments in , appending to list of files to
-    # process, ensuring we expand any Unix-shell globbing-style
-    # arguments
-    for fileName in li:
-      globbedFiles = glob(fileName)
-      if len(globbedFiles) == 0:
-        # if no files were found for that glob, please exit and warn
-        # the user
-        sys.exit("Couldn't find file(s): %s" % fileName)
-      else:
-        fileNames.extend(globbedFiles)
-
-  # parse config file
-  from PyPop.Main import Main, getConfigInstance
-  config = getConfigInstance(configFilename, altpath, usage_message)
-
-  xmlOutPaths = []
-  txtOutPaths = []
-  # loop through list of filenames passed, processing each in turn
-  for fileName in fileNames:
-
-    # parse out the parts of the filename
-    #baseFileName = os.path.basename(fileName)
-
-    application = Main(config=config,
-                       debugFlag=debugFlag,
-                       fileName=fileName,
-                       datapath=datapath,
-                       use_libxsltmod=use_libxsltmod,
-                       use_FourSuite=use_FourSuite,
-                       xslFilename=xslFilename,
-                       xslFilenameDefault=xslFilenameDefault,
-                       outputDir=outputDir,
-                       version=version,
-                       testMode=testMode)
-
-    xmlOutPaths.append(application.getXmlOutPath())
-    txtOutPaths.append(application.getTxtOutPath())
-
-  if generateTSV:
-    from PyPop.Meta import Meta
-    
-    print("Generating TSV (.dat) files...")
-    Meta(popmetabinpath=pypopbinpath,
-         datapath=datapath,
-         metaXSLTDirectory=None,
-         dump_meta=0,
-         R_output=1,
-         PHYLIP_output=0,
-         ihwg_output=1,
-         batchsize=len(xmlOutPaths),
-         files=xmlOutPaths)
-
-  if interactiveFlag:
-
-    print("PyPop run complete!")
-    print("XML output(s) can be found in: ",  xmlOutPaths)
-    print("Plain text output(s) can be found in: ",  txtOutPaths)
-
-    # update .pypoprc file
-
-    if pypoprc.has_section('Files') != 1:
-      pypoprc.add_section('Files')
-      
-    pypoprc.set('Files', 'config', os.path.abspath(configFilename))
-    pypoprc.set('Files', 'pop', os.path.abspath(fileNames[0]))
-    pypoprc.write(open(pypoprcFilename, 'w'))
+  pypoprc.set('Files', 'config', os.path.abspath(configFilename))
+  pypoprc.set('Files', 'pop', os.path.abspath(fileNames[0]))
+  pypoprc.write(open(pypoprcFilename, 'w'))
 
