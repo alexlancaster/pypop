0ed088313f8e8f1c74264f9b1560ef17119e7cf3
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-06-08 02:27:59 -0400

    remove the `-out.txt` output from the comparison
    not needed for the test, and convergence values vary slightly on Windows

--- tests/test_GenerateTSV.py
+++ tests/test_GenerateTSV.py
@@ -11,6 +11,6 @@ def test_GenerateTSV():
     assert exit_code == 0
 
     # compare with output files
-    for out_filename in ['1-locus-allele.dat', '1-locus-pairwise-fnd.dat', '3-locus-summary.dat', '1-locus-genotype.dat', '1-locus-summary.dat', '4-locus-haplo.dat', '1-locus-hardyweinberg.dat', '3-locus-haplo.dat', '4-locus-summary.dat', 'BIGDAWG_SynthControl_Data-out.txt']:
+    for out_filename in ['1-locus-allele.dat', '1-locus-pairwise-fnd.dat', '3-locus-summary.dat', '1-locus-genotype.dat', '1-locus-summary.dat', '4-locus-haplo.dat', '1-locus-hardyweinberg.dat', '3-locus-haplo.dat', '4-locus-summary.dat']:
         gold_out_filename = abspath_test_data(os.path.join('./tests/data/output', out_filename))
         assert filecmp_ignore_newlines(out_filename, gold_out_filename)
