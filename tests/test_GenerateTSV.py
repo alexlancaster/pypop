c82f0777d6d31e0577d7a242e28acf7d1ff5b0dd
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-07-31 00:10:54 -0400

    new unit test for explictly set 2 locus haplotypes

--- tests/test_GenerateTSV.py
+++ tests/test_GenerateTSV.py
@@ -4,7 +4,18 @@ import pytest
 import os.path
 from base import run_pypop_process, filecmp_ignore_newlines, abspath_test_data, xfail_windows
 
+# FIXME: should eventually only include the files relevant for the number of loci
 filenames = ['1-locus-allele.dat', '1-locus-pairwise-fnd.dat', '3-locus-summary.dat', '1-locus-genotype.dat', '1-locus-summary.dat', '4-locus-haplo.dat', '1-locus-hardyweinberg.dat', '3-locus-haplo.dat', '4-locus-summary.dat']
+
+def test_GenerateTSV_2_locus():
+    exit_code = run_pypop_process('./tests/data/WS_BDCtrl_Test_EM_2_locus.ini', './tests/data/BIGDAWG_SynthControl_Data.pop', args=['--enable-tsv', '--enable-ihwg'])
+    # check exit code
+    assert exit_code == 0
+
+    # compare with output files
+    for out_filename in filenames + ['2-locus-summary.dat', '2-locus-haplo.dat']:  # add 2 locus files
+        gold_out_filename = abspath_test_data(os.path.join('./tests/data/output/generate_tsv_2_locus', out_filename))
+        assert filecmp_ignore_newlines(out_filename, gold_out_filename)
 
 def test_GenerateTSV_3_locus():
     exit_code = run_pypop_process('./tests/data/WS_BDCtrl_Test_EM.ini', './tests/data/BIGDAWG_SynthControl_Data.pop', args=['--enable-tsv', '--enable-ihwg'])
