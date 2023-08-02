cb4b48cb1e2e1f2a4f26e84d7ff99bad7db2915a
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-08-02 01:57:10 -0400

    add unit test for HLANomen2010 filters
    modify original .txt output with new output with proper spacing

--- tests/test_Filters.py
+++ tests/test_Filters.py
@@ -27,3 +27,14 @@ def test_Filters_CustomBinning_USAFEL():
 
     assert filecmp_ignore_newlines(out_filename, gold_out_filename)
     
+
+def test_Filters_CustomBinning_HLANomen2010():
+    exit_code = run_pypop_process('./tests/data/HLANomen2010_hap.ini', './tests/data/HLANomen2010_Filter-test.pop')
+    # check exit code
+    assert exit_code == 0
+
+    out_filename = "HLANomen2010_Filter-test-out.txt"
+    gold_out_filename = abspath_test_data(os.path.join('./tests/data/output', out_filename))
+
+    assert filecmp_ignore_newlines(out_filename, gold_out_filename)
+    
