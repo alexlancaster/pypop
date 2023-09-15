09f0324c22b5bd54661f50cec0a2a048bd4a929f
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-09-15 01:21:19 -0400

    only 1-locus and 3-locus files should generated

--- tests/test_Popmeta.py
+++ tests/test_Popmeta.py
@@ -14,11 +14,6 @@ def test_Popmeta():
 
         # compare with output files
         for out_filename in ['1-locus-allele.dat', '1-locus-summary.dat',
-                             #'1-locus-hardyweinberg.dat', 
-                             #'1-locus-pairwise-fnd.dat',
-                             # '1-locus-genotype.dat',
-                             '2-locus-summary.dat', '2-locus-haplo.dat',
-                             '3-locus-summary.dat', '3-locus-haplo.dat',
-                             '4-locus-summary.dat', '4-locus-haplo.dat']:
+                             '3-locus-summary.dat', '3-locus-haplo.dat']:
             gold_out_filename = abspath_test_data(os.path.join('./tests/data/output/popmeta/', out_filename))
             assert filecmp_ignore_newlines(out_filename, gold_out_filename)
