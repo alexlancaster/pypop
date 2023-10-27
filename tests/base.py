f66a24e32782567d6435308bf316e9a8560becef
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-10-27 18:56:23 -0400

    use resolve() to get absolute path

--- tests/base.py
+++ tests/base.py
@@ -182,8 +182,8 @@ def in_temp_dir(request):
         suffix = ''
     )
     os.chdir(test_dir) # change current directory to temp
-    print("curr_dir:", PurePath(curr_dir))
-    print("test_dir:", PurePath(test_dir))
+    print("curr_dir:", Path(curr_dir).resolve())
+    print("test_dir:", Path(test_dir).resolve())
     
     try:
         yield
