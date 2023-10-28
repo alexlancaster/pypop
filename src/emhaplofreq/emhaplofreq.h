5cf7e7893c20f4141827767276033302dbb1976f
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-10-28 04:54:22 -0400

    more accurate implementation of drand48()
    build only cp311 windows build

--- src/emhaplofreq/emhaplofreq.h
+++ src/emhaplofreq/emhaplofreq.h
@@ -71,10 +71,9 @@ MODIFICATIONS. */
 
 /* need to redefine in terms of srand/rand on Windows */
 #if defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__WIN64__)
-#define drand48(x) rand(x)/(RAND_MAX + 1.0)
+#define drand48(x) rand(x)*(1./(RAND_MAX))
 #define srand48(x) srand(x)
 #endif
-
 
 /* 
  * macros to initialize elements of a given static array to `zero'
