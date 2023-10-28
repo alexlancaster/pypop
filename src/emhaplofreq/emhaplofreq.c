c37f57b9643e440255ca7cac527162d0d2d64d48
Alex Lancaster <alexlancaster@users.noreply.github.com>
2023-10-28 05:54:51 -0400

    debug drand48() on windows

--- src/emhaplofreq/emhaplofreq.c
+++ src/emhaplofreq/emhaplofreq.c
@@ -797,7 +797,8 @@ int main_proc(
 	freq_sum = 0;
 	for (i = 0; i < n_haplo; i++)
 	  { 
-	    freq_zero[i] = drand48(); 
+	    freq_zero[i] = drand48();
+	    printf("i=%d, drand48=%g\n", i, freq_zero[i]);
 	    freq_sum += freq_zero[i];
 	  }
 	for (i = 0; i < n_haplo; i++)
