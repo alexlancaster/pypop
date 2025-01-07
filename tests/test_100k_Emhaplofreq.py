d6c41ece55a4c104dba2091e2ae95ed5655e6145
Alex Lancaster <alexlancaster@users.noreply.github.com>
2025-01-07 00:32:38 -0500

    start on pre-commit checks for unit tests
    * exclude `base.py` in `tests/` for name checking

--- tests/test_100k_Emhaplofreq.py
+++ tests/test_100k_Emhaplofreq.py
@@ -1,6 +1,5 @@
-import subprocess
-import hashlib
 import pytest
+
 # FIXME: psutil currently not used, unit test disabled
 # import psutil
 from base import run_pypop_process
@@ -8,14 +7,17 @@ from base import run_pypop_process
 memory_in_gb = 2  # memory needed for this test in GB
 memory_in_bytes = memory_in_gb * (1024 * 1024 * 1024)
 
+
 @pytest.mark.skip(reason="module deprecated")
 ##@pytest.mark.skipif(psutil.virtual_memory().available <= memory_in_bytes, reason="test can only be run if there is sufficient memory on the machine")
 def test_100k_Emhaplofreq():
-    exit_code = run_pypop_process('./tests/data/Test_100k_20loci_Dataset.ini', './tests/data/Test_100k_20loci_Dataset.pop')
+    exit_code = run_pypop_process(
+        "./tests/data/Test_100k_20loci_Dataset.ini",
+        "./tests/data/Test_100k_20loci_Dataset.pop",
+    )
     # check exit code
     assert exit_code == 0
     # compare with md5sum of output file
     # FIXME: disable for the moment
     # with open("Test_100k_20loci_Dataset-out.txt", 'rb') as out_handle:
     #    assert hashlib.md5(out_handle.read()).hexdigest() == 'd899396e90d99ad9eb2002506e699091'
-
--- tests/test_Filters.py
+++ tests/test_Filters.py
@@ -1,57 +1,88 @@
-import sys
-import subprocess
-import hashlib
-import pytest
-import os.path
-from base import abspath_test_data, run_pypop_process, filecmp_ignore_newlines, in_temp_dir, DEFAULT_GOLD_OUTPUT_DIR
+from base import (
+    DEFAULT_GOLD_OUTPUT_DIR,
+    abspath_test_data,
+    filecmp_ignore_newlines,
+    run_pypop_process,
+)
+
 
 def test_Filters_DigitBinning_USAFEL():
-    exit_code = run_pypop_process('./tests/data/Filters_DigitBinning_USAFEL.ini', './tests/data/USAFEL-UchiTelle-small.pop')
+    exit_code = run_pypop_process(
+        "./tests/data/Filters_DigitBinning_USAFEL.ini",
+        "./tests/data/USAFEL-UchiTelle-small.pop",
+    )
     # check exit code
     assert exit_code == 0
 
     out_filename = "USAFEL-UchiTelle-small-Filters_DigitBinning-out.txt"
-    gold_out_filename = abspath_test_data(os.path.join(DEFAULT_GOLD_OUTPUT_DIR, out_filename))
+    gold_out_filename = abspath_test_data(DEFAULT_GOLD_OUTPUT_DIR / out_filename)
 
     assert filecmp_ignore_newlines(out_filename, gold_out_filename)
 
+
 def test_Filters_CustomBinning_USAFEL():
-    exit_code = run_pypop_process('./tests/data/Filters_CustomBinning_USAFEL.ini', './tests/data/USAFEL-UchiTelle-small.pop')
+    exit_code = run_pypop_process(
+        "./tests/data/Filters_CustomBinning_USAFEL.ini",
+        "./tests/data/USAFEL-UchiTelle-small.pop",
+    )
     # check exit code
     assert exit_code == 0
 
     out_filename = "USAFEL-UchiTelle-small-Filters_CustomBinning-out.txt"
-    gold_out_filename = abspath_test_data(os.path.join(DEFAULT_GOLD_OUTPUT_DIR, out_filename))
+    gold_out_filename = abspath_test_data(DEFAULT_GOLD_OUTPUT_DIR / out_filename)
 
     assert filecmp_ignore_newlines(out_filename, gold_out_filename)
 
+
 def test_Filters_CustomBinning_HLANomen2010():
-    exit_code = run_pypop_process('./tests/data/HLANomen2010_hap.ini', './tests/data/HLANomen2010_Filter-test.pop')
+    exit_code = run_pypop_process(
+        "./tests/data/HLANomen2010_hap.ini", "./tests/data/HLANomen2010_Filter-test.pop"
+    )
     # check exit code
     assert exit_code == 0
 
     # check filter log output as well
-    for out_filename in ["HLANomen2010_Filter-test-out.txt", "HLANomen2010_Filter-test-filter.xml"]: 
-        gold_out_filename = abspath_test_data(os.path.join(DEFAULT_GOLD_OUTPUT_DIR, out_filename))
+    for out_filename in [
+        "HLANomen2010_Filter-test-out.txt",
+        "HLANomen2010_Filter-test-filter.xml",
+    ]:
+        gold_out_filename = abspath_test_data(DEFAULT_GOLD_OUTPUT_DIR / out_filename)
         assert filecmp_ignore_newlines(out_filename, gold_out_filename)
-    
+
+
 def test_Filters_CustomBinning_G_Filter():
-    exit_code = run_pypop_process('./tests/data/custom-binning-examples/G-Filter_config.ini', './tests/data/BIGDAWG_SynthControl_Data.pop')
+    exit_code = run_pypop_process(
+        "./tests/data/custom-binning-examples/G-Filter_config.ini",
+        "./tests/data/BIGDAWG_SynthControl_Data.pop",
+    )
     # check exit code
     assert exit_code == 0
 
     # check filter log output as well
-    for out_filename in ["BIGDAWG_SynthControl_Data-filter.xml", "BIGDAWG_SynthControl_Data-out.txt"]:
-        gold_out_filename = abspath_test_data(os.path.join(DEFAULT_GOLD_OUTPUT_DIR, "custom-binning-G-Filter", out_filename))
+    for out_filename in [
+        "BIGDAWG_SynthControl_Data-filter.xml",
+        "BIGDAWG_SynthControl_Data-out.txt",
+    ]:
+        gold_out_filename = abspath_test_data(
+            DEFAULT_GOLD_OUTPUT_DIR / "custom-binning-G-Filter" / out_filename
+        )
         assert filecmp_ignore_newlines(out_filename, gold_out_filename)
+
 
 def test_Filters_CustomBinning_P_Filter():
-    exit_code = run_pypop_process('./tests/data/custom-binning-examples/P-Filter_config.ini', './tests/data/BIGDAWG_SynthControl_Data.pop')
+    exit_code = run_pypop_process(
+        "./tests/data/custom-binning-examples/P-Filter_config.ini",
+        "./tests/data/BIGDAWG_SynthControl_Data.pop",
+    )
     # check exit code
     assert exit_code == 0
 
     # check filter log output as well
-    for out_filename in ["BIGDAWG_SynthControl_Data-filter.xml", "BIGDAWG_SynthControl_Data-out.txt"]:
-        gold_out_filename = abspath_test_data(os.path.join(DEFAULT_GOLD_OUTPUT_DIR, "custom-binning-P-Filter", out_filename))
+    for out_filename in [
+        "BIGDAWG_SynthControl_Data-filter.xml",
+        "BIGDAWG_SynthControl_Data-out.txt",
+    ]:
+        gold_out_filename = abspath_test_data(
+            DEFAULT_GOLD_OUTPUT_DIR / "custom-binning-P-Filter" / out_filename
+        )
         assert filecmp_ignore_newlines(out_filename, gold_out_filename)
-        
