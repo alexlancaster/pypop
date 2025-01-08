4c6a5c88142c6c48805eba658b15d82f92dfdc67
Alex Lancaster <alexlancaster@users.noreply.github.com>
2025-01-08 02:13:27 -0500

    `ruff` formatting

--- src/haplo-stats/test1.py
+++ src/haplo-stats/test1.py
@@ -5,7 +5,7 @@ import string
 import numpy
 
 DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(DIR, '..'))
+sys.path.insert(0, os.path.join(DIR, ".."))
 
 from PyPop.Utils import StringMatrix
 from PyPop.Haplo import Haplostats
@@ -31,26 +31,28 @@ from PyPop.Haplo import Haplostats
 # so we set StringMatrix to be:
 
 geno = StringMatrix(5, ["DRB", "B"])
-geno[0, 'DRB'] = ('4', '11')
-geno[1, 'DRB'] = ('2', '7')
-geno[2, 'DRB'] = ('1', '13')
-geno[3, 'DRB'] = ('7', '7')
-geno[4, 'DRB'] = ('8', '11')
-geno[0, 'B'] = ('62', '61')
-geno[1, 'B'] = ('7', '44')
-geno[2, 'B'] = ('27', '62')
-geno[3, 'B'] = ('7', '44')
-geno[4, 'B'] = ('51', '55')
+geno[0, "DRB"] = ("4", "11")
+geno[1, "DRB"] = ("2", "7")
+geno[2, "DRB"] = ("1", "13")
+geno[3, "DRB"] = ("7", "7")
+geno[4, "DRB"] = ("8", "11")
+geno[0, "B"] = ("62", "61")
+geno[1, "B"] = ("7", "44")
+geno[2, "B"] = ("27", "62")
+geno[3, "B"] = ("7", "44")
+geno[4, "B"] = ("51", "55")
 
 # set all the control parameters
 # possibly move this into the .ini file eventually?
-control = {'max_iter': 5000,
-           'min_posterior': 0.000000001,
-           'tol': 0.00001,
-           'insert_batch_size': 2,
-           'random_start': 0,
-           'verbose': 0,
-           'max_haps_limit': 10000 }
+control = {
+    "max_iter": 5000,
+    "min_posterior": 0.000000001,
+    "tol": 0.00001,
+    "insert_batch_size": 2,
+    "random_start": 0,
+    "verbose": 0,
+    "max_haps_limit": 10000,
+}
 
 # FIXME: currently this assumes that geno StringMatrix contains only the loci required
 # need to make sure that this works with subMatrices
@@ -61,8 +63,24 @@ from PyPop.Utils import XMLOutputStream
 xmlOutput = XMLOutputStream(io.StringIO())
 
 haplo = Haplostats(geno, stream=xmlOutput)
-converge, lnlike, n_u_hap, n_hap_pairs, hap_prob, u_hap, u_hap_code, subj_id, post, hap1_code, hap2_code, haplotype, dprime, Wn, ALD_1_2, ALD_2_1 = \
-          haplo.estHaplotypes(weight=None, control=control, numInitCond=1)
+(
+    converge,
+    lnlike,
+    n_u_hap,
+    n_hap_pairs,
+    hap_prob,
+    u_hap,
+    u_hap_code,
+    subj_id,
+    post,
+    hap1_code,
+    hap2_code,
+    haplotype,
+    dprime,
+    Wn,
+    ALD_1_2,
+    ALD_2_1,
+) = haplo.estHaplotypes(weight=None, control=control, numInitCond=1)
 
 print(" converge:", converge)
 print(" lnlike:", lnlike)
@@ -78,10 +96,10 @@ print(" hap2_code:", hap2_code)
 
 # Print columns side-by-side for easier checking
 # NB: u_hap is trickier since it has n.loci entries per haplo
-print('hap_prob  u_hap_code u_hap(needs to be split for printing)')
-print(numpy.c_[hap_prob,u_hap_code])
-print('subj_id  hap1_code  hap2_code')
-print(numpy.c_[subj_id,hap1_code,hap2_code])
+print("hap_prob  u_hap_code u_hap(needs to be split for printing)")
+print(numpy.c_[hap_prob, u_hap_code])
+print("subj_id  hap1_code  hap2_code")
+print(numpy.c_[subj_id, hap1_code, hap2_code])
 #   for x1,x2,x3 in zip(hap_prob,u_hap,u_hap_code):
 #       print x1 + '\t\t' + x2 + '\t\t' + x3
 
--- src/haplo-stats/test3.py
+++ src/haplo-stats/test3.py
@@ -6,13 +6,15 @@ import sys
 import itertools as it
 
 DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(DIR, '..'))
+sys.path.insert(0, os.path.join(DIR, ".."))
 
 from PyPop.Haplo import _compute_LD
 
 # FIXME: these arrays have to be in same order
 # this is fragile and probably needs changing in main code
-haplos = numpy.array([['A1', 'B1'], ['A2', 'B1'], ['A1', 'B2'], ['A2', 'B2']],dtype='O')
+haplos = numpy.array(
+    [["A1", "B1"], ["A2", "B1"], ["A1", "B2"], ["A2", "B2"]], dtype="O"
+)
 freqs = numpy.array([0.3, 0.1, 0.1, 0.5])
 
 _compute_LD(haplos, freqs, compute_ALD=True, debug=True)
