ac0e37b8068e1546159722a7b8be40a7f7fac0c3
Alex Lancaster <alexlancaster@users.noreply.github.com>
2025-02-05 05:50:32 -0500

    Add support for optional computation of `pval` using `scipy` and benchmarking tests (#257)
    * compiled `pval` is still the default, can be manually enabled by editing `use_scipy` boolean in `HardyWeinberg` module
    
    * add `--pval-benchmarking` option to `pytest` that compares `scipy` pval vs compiled `pval`
    
    * only run pvalue tests if `scipy` is installed AND `--pval-benchmarking` is set
    
    * move `pytest-benchmark` into default test deps
    
    * run the benchmarking only on Linux ARM64 for Python 3.13
    
    * move `test-command` logic into `pyproject.toml` out of workflow
    
    * fix the Windows `test-command`

--- tests/test_GenotypeCommon.py
+++ tests/test_GenotypeCommon.py
@@ -1,9 +1,47 @@
 import hashlib
+from unittest import mock
 
+import pytest
 from base import (
     in_temp_dir,  # noqa: F401
     run_pypop_process,
 )
+
+
+@pytest.mark.pval_benchmarking
+def test_GenotypeCommon_HardyWeinberg_scipy_pval(benchmark):
+    with mock.patch("PyPop.HardyWeinberg.use_scipy", True):
+        exit_code = benchmark(
+            run_pypop_process,
+            "./tests/data/WS_BDCtrl_Test_HW.ini",
+            "./tests/data/BIGDAWG_SynthControl_Data.pop",
+        )
+
+        # check exit code
+        assert exit_code == 0
+        # compare with md5sum of output file
+        with open("BIGDAWG_SynthControl_Data-out.txt", "rb") as out_handle:
+            assert (
+                hashlib.md5(out_handle.read()).hexdigest()
+                == "099138eb81d9fd1ef8a3e9cde8fa1e60"
+            )
+
+
+@pytest.mark.pval_benchmarking
+def test_GenotypeCommon_HardyWeinberg_compiled_pval(benchmark):
+    exit_code = benchmark(
+        run_pypop_process,
+        "./tests/data/WS_BDCtrl_Test_HW.ini",
+        "./tests/data/BIGDAWG_SynthControl_Data.pop",
+    )
+    # check exit code
+    assert exit_code == 0
+    # compare with md5sum of output file
+    with open("BIGDAWG_SynthControl_Data-out.txt", "rb") as out_handle:
+        assert (
+            hashlib.md5(out_handle.read()).hexdigest()
+            == "099138eb81d9fd1ef8a3e9cde8fa1e60"
+        )
 
 
 def test_GenotypeCommon_HardyWeinberg():
---
+++ tests/test_Pvalue_testing.py
@@ -0,0 +1,33 @@
+# from base import abspath_test_data, run_pypop_process, filecmp_ignore_newlines, in_temp_dir, DEFAULT_GOLD_OUTPUT_DIR
+from unittest import mock
+
+import pytest
+
+chisq = 10.3
+dof = 2
+# expected = 0.0013303020906467733 for 1 dof
+expected = 0.00579940472684215  # for 2 dof
+
+pytestmark = (
+    pytest.mark.pval_benchmarking
+)  # applies the marker to all tests in this file
+
+
+def test_SciPyPval(benchmark):
+    # force using scipy
+    with mock.patch("PyPop.HardyWeinberg.use_scipy", True):
+        from PyPop.HardyWeinberg import pval
+
+        result = benchmark(pval, chisq, dof)
+        print(result)
+
+    assert result == expected
+
+
+def test_BuiltInPvalue(benchmark):
+    from PyPop.HardyWeinberg import pval
+
+    result = benchmark(pval, chisq, dof)
+    print(result)
+
+    assert result == expected
