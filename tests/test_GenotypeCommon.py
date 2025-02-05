e0f5a6bba3b8b59c063fb1fe6f5f10f06cd3b0e4
Alex Lancaster <alexlancaster@users.noreply.github.com>
2025-02-04 20:47:31 -0500

    update MD5 sums for unit test changed output

--- tests/test_GenotypeCommon.py
+++ tests/test_GenotypeCommon.py
@@ -1,30 +1,44 @@
 import hashlib
-import pytest
 from unittest import mock
+
 from base import (
     in_temp_dir,  # noqa: F401
     run_pypop_process,
 )
 
-def test_GenotypeCommon_HardyWeinberg_scipy_pval(benchmark):
 
-    with mock.patch('PyPop.HardyWeinberg.use_scipy', True) as new_scipy:    
-        exit_code = benchmark(run_pypop_process, './tests/data/WS_BDCtrl_Test_HW.ini', './tests/data/BIGDAWG_SynthControl_Data.pop')
-        
+def test_GenotypeCommon_HardyWeinberg_scipy_pval(benchmark):
+    with mock.patch("PyPop.HardyWeinberg.use_scipy", True):
+        exit_code = benchmark(
+            run_pypop_process,
+            "./tests/data/WS_BDCtrl_Test_HW.ini",
+            "./tests/data/BIGDAWG_SynthControl_Data.pop",
+        )
+
         # check exit code
         assert exit_code == 0
         # compare with md5sum of output file
-        with open("BIGDAWG_SynthControl_Data-out.txt", 'rb') as out_handle:
-            assert hashlib.md5(out_handle.read()).hexdigest() == '7162aa0715ccfd1cb7b666409d839129'
+        with open("BIGDAWG_SynthControl_Data-out.txt", "rb") as out_handle:
+            assert (
+                hashlib.md5(out_handle.read()).hexdigest()
+                == "099138eb81d9fd1ef8a3e9cde8fa1e60"
+            )
+
 
 def test_GenotypeCommon_HardyWeinberg_compiled_pval(benchmark):
-    exit_code = benchmark(run_pypop_process, './tests/data/WS_BDCtrl_Test_HW.ini', './tests/data/BIGDAWG_SynthControl_Data.pop')
+    exit_code = benchmark(
+        run_pypop_process,
+        "./tests/data/WS_BDCtrl_Test_HW.ini",
+        "./tests/data/BIGDAWG_SynthControl_Data.pop",
+    )
     # check exit code
     assert exit_code == 0
     # compare with md5sum of output file
-    with open("BIGDAWG_SynthControl_Data-out.txt", 'rb') as out_handle:
-        assert hashlib.md5(out_handle.read()).hexdigest() == '7162aa0715ccfd1cb7b666409d839129'
-        
+    with open("BIGDAWG_SynthControl_Data-out.txt", "rb") as out_handle:
+        assert (
+            hashlib.md5(out_handle.read()).hexdigest()
+            == "099138eb81d9fd1ef8a3e9cde8fa1e60"
+        )
 
 
 def test_GenotypeCommon_HardyWeinberg():
