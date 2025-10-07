c8cb7c357d7394216327366d00696a406856cc22
Alex Lancaster <alexlancaster@users.noreply.github.com>
2025-10-06 22:58:47 -0400

    docstring tweak

--- src/PyPop/__init__.py
+++ src/PyPop/__init__.py
@@ -44,10 +44,13 @@ as Python modules and classes.
    developers who are working on PyPop itself.
 
 It is possible, however, to drive PyPop programmatically primarily via
-the :mod:`PyPop.Main` module. For example, here we instantiate a
-:class:`PyPop.Main.Main` object with a configuration instance with the
-default settings, and one analysis enabled, together with an input
-``.pop`` file, and use this to generate output.  We first create the
+the :mod:`PyPop.Main` module.
+
+Example:
+Here we instantiate a :class:`PyPop.Main.Main` object with a
+configuration instance with the default settings, and one analysis
+enabled, together with an input ``.pop`` file, and use this to
+generate output.  We first create the
 :class:`configparser.ConfigParser` instance (see :ref:`configuration
 file section <guide-usage-configfile>` in the *PyPop User Guide* for
 the description of the configuration options), supply this to the
